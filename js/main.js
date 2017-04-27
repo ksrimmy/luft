28b9c1a6f34ea955431fc8f4273dfe5c92e40f7e
Florian Steiner <florian.steiner@cgm.com>
2017-04-27 14:56:50 +0200

    added PM10 and PM2,5

--- js/main.js
+++ js/main.js
@@ -17,3 +17,22 @@ $.ajax({
     }
 
 });
+$.ajax({
+    url: "http://api.luftdaten.info/v1/sensor/1358/",
+    type: "GET",
+    cache: false,
+    dataType: "json"
+}).done(function(data) {
+    var pm10 = $("#pm10");
+    var pm25 = $("#pm25");
+    console.log(data);
+
+    for(var i = 0; i < 5; i++) {
+        if (data[i].sensordatavalues.length > 1) {
+            pm10.append(data[i].sensordatavalues[0].value + " µg/m³");
+            pm25.append(data[i].sensordatavalues[1].value + " µg/m³");
+            break;
+        }
+    }
+
+});
