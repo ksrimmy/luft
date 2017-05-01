718930143f0522c91ee92cb07ae8adfaf4ab7220
Florian Steiner <flo@static.196.46.9.176.clients.your-server.de>
2017-05-01 20:26:21 +0200

    fixed uncaught typeerror

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
+$( document ).ready(function() {
 $.ajax({
-    url: "http://api.luftdaten.info/v1/sensor/1359/",
+    url: "https://api.luftdaten.info/v1/sensor/1359/",
     type: "GET",
     cache: false,
     dataType: "json"
 }).done(function(data) {
-    var temperautre = $("#temperature");
-    var humidity = $("#humidity");
     console.log(data);
 
     for(var i = 0; i < 5; i++) {
         if (data[i].sensordatavalues.length > 1) {
-            temperature.append(data[i].sensordatavalues[0].value + " °C");
-            humidity.append(data[i].sensordatavalues[1].value + " %");
+            document.getElementById("temperature").innerHTML = data[i].sensordatavalues[0].value + " °C";
+            document.getElementById("humidity").innerHTML = data[i].sensordatavalues[1].value + " %";
             break;
         }
     }
 
 });
 $.ajax({
-    url: "http://api.luftdaten.info/v1/sensor/1358/",
+    url: "https://api.luftdaten.info/v1/sensor/1358/",
     type: "GET",
     cache: false,
     dataType: "json"
 }).done(function(data) {
-    var pm10 = $("#pm10");
-    var pm25 = $("#pm25");
     console.log(data);
 
     for(var i = 0; i < 5; i++) {
         if (data[i].sensordatavalues.length > 1) {
-            pm10.append(data[i].sensordatavalues[0].value + " µg/m³");
-            pm25.append(data[i].sensordatavalues[1].value + " µg/m³");
+            document.getElementById("pm10").innerHTML =data[i].sensordatavalues[0].value + " µg/m³";
+            document.getElementById("pm25").innerHTML = data[i].sensordatavalues[1].value + " µg/m³";
             break;
         }
     }
 
+});
 });
